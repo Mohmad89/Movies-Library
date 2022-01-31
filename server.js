ab7cd5c944907dfb664ba46889a9429d39982ef3
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-01-31 23:17:23 +0200

    Task 12 Upload

--- server.js
+++ server.js
@@ -1,35 +1,72 @@
 
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
+
 const fav_data = require('./MovieData/data.json');
 
+const PORT = process.env.PORT;
+const APIKEY = process.env.APIKEY;
+
 const server = express();
-server.unsubscribe(cors());
+server.use(cors());
 
-server.get('/' , handelGet );
-server.get('/favorite' , handelGetFav);
-server.get('*' , handelGetEror);
+server.get('/trending' , handelGetTrnding );
+server.get('/search' , handelGetSearch);
+server.use('*',notFoundHandler);
+server.use(errorHandler);
 
-function constr(title, poster_path, overview) {
+function Movi(id , title, release_date , poster_path, overview) {
+    this.id = id ;
     this.title = title;
+    this.release_date = release_date;
     this.poster_path = poster_path;
     this.overview = overview;
 }
 
-function handelGet( requ , resp){
-    return resp.status(200).send("Empty page");
-}
-function handelGetFav(req , res){
-    let data = fav_data.data.map(val =>{
-        return new constr(val.title , val.poster_path , val.overview);
+let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`
+
+function handelGetTrnding(req , res){
+    let newArr = [];
+    axios.get(url)
+    .then((result) => {
+        result.data.results.forEach((result) => {
+            newArr.push(new Movi(result.id , result.title, result.release_date, result.poster_path , result.overview));
+        })
+        res.status(200).json(newArr);
+    })
+    .catch((error) => {
+        errorHandler(error , req , res);
     });
-    res.status(200).json(data);
+}
+let movi_fav = 'The%20Marksman';
+function handelGetSearch(req , res){
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&id=756403&query=${movi_fav}`;
+    axios.get(url)
+    .then(result => {
+        let movies = result.data.results.map(movie => {
+            return new Movi(movie.id, movie.title, movie.release_date , movie.poster_path, movie.overview);
+        });
+        res.status(200).json(movies);
+    }).catch((error)=>{
+                errorHandler(error , req , res);
+
+    })
 }
 
-function handelGetEror(req , res){
-    return res.status(404).send("Page Not Found Eror 404 ");
+function notFoundHandler(req,res){
+   res.status(404).send("This page is not found")
+}
+function errorHandler(error , req , res){
+    const err =   {
+        status:500 , 
+        message : error
+        }
+    res.status(500).send(err);
 }
 
-server.listen(3000 , ()=>{
+//server listen
+server.listen(PORT , ()=>{
     console.log("listen is start");
 })
