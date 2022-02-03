6e2e5e7da5b9cb8aa9030a7409cba4dc53fe3959
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-02-03 02:51:47 +0200

    Task 13 Upload

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
-DROP TABLE IF EXISTS AddMovies;
+DROP TABLE IF EXISTS addmovies;
 
-CREATE TABLE IF NOT EXISTS AddMovies(
+CREATE TABLE IF NOT EXISTS addmovies(
     id SERIAL PRIMARY KEY ,
     title VARCHAR(255) , 
     release_date VARCHAR(255) ,
     poster_path VARCHAR(255) ,
-    overview VARCHAR(255) 
+    overview VARCHAR(255) ,
+    commint VARCHAR(255) 
 );
