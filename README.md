15fa6fffa2e017bcc28528477bc83efb538b09a4
Mohmad89 <mohmadadarbeh7@gmail.com>
2022-02-09 00:28:37 +0200

    Resupmit Task11

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Movies-Library
+# 
+
+# Movies-Library - 1
+
+**Author Name**: Mohammad emad aladarbeh
+
+## WRRC  Features
+![](wrrc.png)
+
+
+## Overview
+
+## Getting Started
+- user must install library that is required such as:  
+    1. npm i express cors  
+- user must call this library with use require('name Of library')  
+- create server  
+- call the point such as /favorite  
+- get url for your websit 
+- get the key 
+- call listin function to run the server  
+- write npm start inside terminal to run server    
+
+
+## Project Features
+- get the value from the data file
+- connect between server and my project
--- server.js
+++ server.js
@@ -1,33 +1,49 @@
 
 const express = require('express');
 const cors = require('cors');
-const fav_data = require('./MovieData/data.json');
+const data = require('./MovieData/data.json');
+const APIKEY = '2b7e0d0fc3aa48406f2242c8379da8ed';
 
 const server = express();
-server.unsubscribe(cors());
+server.use(cors());
+server.use(express.json());
 
 server.get('/' , handelGet );
 server.get('/favorite' , handelGetFav);
 server.get('*' , handelGetEror);
+server.use(errorHandler);
 
-function constr(title, poster_path, overview) {
+// ***************************    constructor     ********************************
+
+function Constr(title, poster_path, overview) {
     this.title = title;
     this.poster_path = poster_path;
     this.overview = overview;
 }
 
-function handelGet( requ , resp){
-    return resp.status(200).send("Empty page");
+// ***************************       functions  ********************************
+
+let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`
+
+function handelGet( req , res){
+    let obj = {title: data.title, poster_path: data.poster_path , overview: data.overview};
+    return res.status(200).json(obj);
 }
 function handelGetFav(req , res){
-    let data = fav_data.data.map(val =>{
-        return new constr(val.title , val.poster_path , val.overview);
-    });
-    res.status(200).json(data);
+    
+    return res.status(200).send("Welcome to Favorite Page");
 }
 
 function handelGetEror(req , res){
     return res.status(404).send("Page Not Found Eror 404 ");
+}
+
+function errorHandler (error,req,res){
+    const err = {
+        status : 500,
+        messgae : error
+    }
+    res.status(500).send(err);
 }
 
 server.listen(3000 , ()=>{
